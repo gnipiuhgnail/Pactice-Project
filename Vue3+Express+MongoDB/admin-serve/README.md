0872b88c42166e2db4e706e59f06a43d87e68450
harper-liang <1960511611@qq.com>
2021-12-10 17:10:59 +0800

    init

---
+++ Vue3+Express+MongoDB/admin-serve/README.md
@@ -0,0 +1,16 @@
+###### admin
+创建项目：vue create admin
+下载安装：npm install
+运行项目：npm run serve 
+element框架：npm install element-plus --save
+axios库：npm install axios --save
+
+###### serve
+npm install express --save
+npm init
+<!-- 全局安装express应用生成器，类似于vue-cli -->
+npm install -g express-generator
+express express-serve 生成项目
+cd express-serve
+npm install
+SET DEBUG=express-serve:* & npm start / npm start
---
+++ Vue3+Express+MongoDB/admin-serve/admin-vue3-js/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-aside width="200px">
+        <el-menu :default-openeds="['1', '3']">
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2" @click="$router.push('/firstchildren')"
+            >选项2</el-menu-item
+          >
+        </el-menu>
+      </el-aside>
+      <el-main><router-view></router-view></el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang="scss" scoped>
+.el-menu {
+  border: 1px solid #ccc;
+  .el-menu-item {
+    display: block;
+    text-align: left;
+  }
+}
+</style>
